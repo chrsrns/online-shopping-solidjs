9b1e1313435cbd8de8b03095ac32149631fca1ed
Christian Aranas <aranaschristianlouise@gmail.com>
2024-05-25 19:47:28 +0800

    Added a placeholder hero for the main page
    This is the rough outline of what I want the hero to be; with no animations yet.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,23 @@ import logo from "./logo.svg";
 const App: Component = () => {
   return (
     <div class="text-center">
+      <div class="relative h-screen w-full">
+        <img
+          src="https://images.pexels.com/photos/271816/pexels-photo-271816.jpeg"
+          alt="Background Image"
+          class="object-cover object-center w-full h-full"
+        />
+        <img
+          src=""
+          alt="Background Image"
+          class="absolute inset-0 w-full h-full object-cover filter blur-sm"
+        />
+        <div class="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div class="absolute inset-0 flex flex-col items-center justify-center">
+          <h1 class="text-4xl text-white font-bold">Hello, World!</h1>
+          <p class="text-xl text-white mt-4">This is a text</p>
+        </div>
+      </div>
       <header class="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-[calc(10px+2vmin)] text-white">
         <img
           src={logo}
