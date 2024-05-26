e68fe56efd54cb14d3b8d664f367940094efb381
Christian Aranas <aranaschristianlouise@gmail.com>
2024-05-26 18:05:07 +0800

    Changed the alignment of the text in the about section on some screen sizes

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App: Component = () => {
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
           <div class="grid items-center grid-cols-1 md:grid-cols-2">
             <div>
-              <h2 class="text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl lg:text-5xl">
+              <h2 class="text-3xl font-bold leading-tight text-black text-center dark:text-white sm:text-4xl md:text-left lg:text-5xl">
                 Elevate your most&nbsp;
                 <br class="block sm:hidden" />
                 important space
@@ -37,7 +37,7 @@ const App: Component = () => {
                 Exercitation veniam consequat sunt nostrud amet.
               </p>
 
-              <p class="mt-4 mb-8 text-xl text-gray-600 dark:text-gray-300 md:mt-8">
+              <p class="mt-4 mb-8 text-xl text-center md:text-left text-gray-600 dark:text-gray-300 md:mt-8">
                 <span class="relative inline-block">
                   <span class="absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300 dark:bg-gray-900"></span>
                   <span class="relative">
