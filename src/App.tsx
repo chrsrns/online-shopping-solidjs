5ec8bb84a85dacbdfa2bc5f1f1ee9f32e67cfb95
Christian Aranas <aranaschristianlouise@gmail.com>
2024-05-26 18:03:18 +0800

    Added a placeholder carousel for the main page
    The final version of this component will contain the reviews or testimonials of 'customers'.

--- src/App.tsx
+++ src/App.tsx
@@ -64,6 +64,34 @@ const App: Component = () => {
           </div>
         </div>
       </section>
+      <main class="snap-center">
+        <div class="w-full h-screen py-8 px-8 overflow-x-auto flex gap-2 items-center snap-x snap-mandatory">
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            1
+          </div>
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            2
+          </div>
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            3
+          </div>
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            4
+          </div>
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            5
+          </div>
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            6
+          </div>
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            7
+          </div>
+          <div class="h-full w-full flex-grow-1 basis-full flex-shrink-0 flex flex-col justify-center items-center bg-[#bae] snap-center">
+            8
+          </div>
+        </div>
+      </main>
       <header class="bg-[#282c34] min-h-screen flex flex-col items-center justify-center text-[calc(10px+2vmin)] text-white">
         <img
           src={logo}
