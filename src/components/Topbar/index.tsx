7cc3c9fcbeb75c16c37700ac2d1a7b70bb11c6c0
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-11 17:45:57 +0800

    Updated layout of topbar
    Center contents on smaller screens.
    Make logo yellow.
    Increase font size of items.

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -1,8 +1,8 @@
 const Topbar = () => {
   return (
     <header class="bg-white">
-      <div class="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-        <a class="block text-teal-600" href="#">
+      <div class="mx-auto flex h-16 max-w-screen-xl items-center justify-center md:justify-start gap-8 px-4 sm:px-6 lg:px-8">
+        <a class="block text-yellow-400" href="#">
           <span class="sr-only">Home</span>
           <svg
             class="h-8"
@@ -17,12 +17,12 @@ const Topbar = () => {
           </svg>
         </a>
 
-        <div class="flex flex-1 items-center justify-end md:justify-between">
-          <nav aria-label="Global" class="hidden md:block">
+        <div class="flex items-center">
+          <nav aria-label="Global">
             <ul class="flex items-center gap-6 text-sm">
               <li>
                 <a
-                  class="text-gray-500 transition hover:text-gray-500/75"
+                  class="text-gray-500 text-base transition hover:text-gray-500/75"
                   href="#"
                 >
                   {" "}
@@ -32,7 +32,7 @@ const Topbar = () => {
 
               <li>
                 <a
-                  class="text-gray-500 transition hover:text-gray-500/75"
+                  class="text-gray-500 text-base transition hover:text-gray-500/75"
                   href="#"
                 >
                   {" "}
@@ -42,7 +42,7 @@ const Topbar = () => {
 
               <li>
                 <a
-                  class="text-gray-500 transition hover:text-gray-500/75"
+                  class="text-gray-500 text-base transition hover:text-gray-500/75"
                   href="#"
                 >
                   {" "}
@@ -51,6 +51,25 @@ const Topbar = () => {
               </li>
             </ul>
           </nav>
+        </div>
+        <div class="flex items-center gap-4 hidden">
+          <button class="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 ">
+            <span class="sr-only">Toggle menu</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </header>
