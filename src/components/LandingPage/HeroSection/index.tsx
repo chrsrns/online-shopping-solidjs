1c75e3737fdebfb7281ae7bd2839401b3e80ae4d
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-20 19:26:18 +0800

    Formatted `HeroSection`
    Rearranged classes.

--- src/components/LandingPage/HeroSection/index.tsx
+++ src/components/LandingPage/HeroSection/index.tsx
@@ -5,17 +5,17 @@ const LandingHeroSection = () => {
         <img
           src="https://images.pexels.com/photos/271816/pexels-photo-271816.jpeg"
           alt="Background Image"
-          class="object-cover object-center w-full h-full"
+          class="h-full w-full object-cover object-center"
         />
         <img
           src=""
           alt="Background Image"
-          class="absolute inset-0 w-full h-full object-cover filter blur-sm"
+          class="absolute inset-0 h-full w-full object-cover blur-sm filter"
         />
         <div class="absolute inset-0 bg-black bg-opacity-50"></div>
         <div class="absolute inset-0 flex flex-col items-center justify-center">
-          <h1 class="text-4xl text-white font-bold">Hello, World!</h1>
-          <p class="text-xl text-white mt-4">This is a text</p>
+          <h1 class="text-4xl font-bold text-white">Hello, World!</h1>
+          <p class="mt-4 text-xl text-white">This is a text</p>
         </div>
       </div>
     </div>
