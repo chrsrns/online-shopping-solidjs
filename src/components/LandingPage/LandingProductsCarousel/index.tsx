c0fb099d4cf2d3e34995906f80b37ab7ec3d0218
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-20 08:28:22 +0800

    Changed colors on `LandingProductsCarousel`
    Themed in accordance to custom color palette.
    Use For loop to create the items.

--- src/components/LandingPage/LandingProductsCarousel/index.tsx
+++ src/components/LandingPage/LandingProductsCarousel/index.tsx
@@ -1,31 +1,16 @@
+import { For } from "solid-js";
+
 const LandingProductsCarousel = () => {
   return (
     <main class="snap-center">
-      <div class="flex h-screen w-full snap-x snap-mandatory items-center gap-2 overflow-x-auto px-8 py-8">
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          1
-        </div>
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          2
-        </div>
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          3
-        </div>
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          4
-        </div>
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          5
-        </div>
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          6
-        </div>
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          7
-        </div>
-        <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center bg-[#bae]">
-          8
-        </div>
+      <div class="flex h-screen w-full snap-x snap-mandatory items-center gap-8 overflow-x-auto bg-timberwolf-900 px-16 py-8 dark:bg-dim_gray">
+        <For each={[...Array(10).keys()]}>
+          {(item, index) => (
+            <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center rounded-md bg-timberwolf dark:bg-black_olive dark:text-white">
+              {item}
+            </div>
+          )}
+        </For>
       </div>
     </main>
   );
