74d2792e64c7b154c74cd05320541512b8d57185
Christian Aranas <aranaschristianlouise@gmail.com>
2024-06-22 12:31:47 +0800

    Implement animation in LandingAnimationSection
    Extend the HTMLImageElement prototype with a cascading function.
    Use `throttle` on the scroll listener.
    Add additional classes to the images.

--- src/components/LandingPage/LandingAnimationSection/index.tsx
+++ src/components/LandingPage/LandingAnimationSection/index.tsx
@@ -1,10 +1,27 @@
+import { createEffect, createSignal } from "solid-js";
 import { ComponentRefProps } from "../../../ComponentRefProps";
 
 interface LandingAnimationSectionProps extends ComponentRefProps {
   animate: boolean;
 }
 
+declare global {
+  interface HTMLImageElement {
+    replaceClassName(oldToken: string, newToken: string): HTMLImageElement;
+  }
+}
+
+HTMLImageElement.prototype.replaceClassName = function (
+  oldToken: string,
+  newToken: string,
+): HTMLImageElement {
+  this.classList.replace(oldToken, newToken);
+  return this;
+};
+
 const LandingAnimationSection = (props: LandingAnimationSectionProps) => {
+  const [didAnimate, setDidAnimate] = createSignal(false);
+
   let furn_chair!: HTMLImageElement;
   let furn_worktable!: HTMLImageElement;
   let furn_bed!: HTMLImageElement;
@@ -16,8 +33,76 @@ const LandingAnimationSection = (props: LandingAnimationSectionProps) => {
   let furn_wallshelf!: HTMLImageElement;
   let furn_wallstickies!: HTMLImageElement;
 
+  const opacity_0 = "opacity-0";
+  const opacity_1 = "opacity-100";
+  const translate_r_0 = "translate-x-0";
+  const translate_r_52 = "translate-x-32";
+  const translate_l_0 = "-translate-x-0";
+  const translate_l_52 = "-translate-x-32";
+
+  createEffect(() => {
+    if (props.animate && !didAnimate()) {
+      setDidAnimate(true);
+      console.log("animate now");
+
+      const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+      (async () => {
+        furn_chair
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_worktable
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_bed
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_macpc
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_shelfdecor
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_tablelamp
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_wallbookshelf
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_wallpainting
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_wallshelf
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+        furn_wallstickies
+          .replaceClassName(opacity_0, opacity_1)
+          .replaceClassName(translate_r_52, translate_r_0)
+          .replaceClassName(translate_l_52, translate_l_0);
+        await delay(150);
+      })();
+    }
+  });
+
   return (
-    <div class="snap-center">
+    <div class="snap-center" id="animation">
       <section
         ref={props.ref}
         class="flex flex-col items-center justify-center overflow-hidden bg-walnut_brown-800 py-8 sm:pt-16 md:pt-0 2xl:pt-16 dark:bg-walnut_brown-300"
@@ -62,61 +147,61 @@ const LandingAnimationSection = (props: LandingAnimationSectionProps) => {
                 />
                 <img
                   ref={furn_chair}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_l_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-chair.png"
                   alt=""
                 />
                 <img
                   ref={furn_worktable}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_r_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-worktable.png"
                   alt=""
                 />
                 <img
                   ref={furn_bed}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_l_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-bed.png"
                   alt=""
                 />
                 <img
                   ref={furn_macpc}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_r_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-macpc.png"
                   alt=""
                 />
                 <img
                   ref={furn_shelfdecor}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_l_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-shelfdecor.png"
                   alt=""
                 />
                 <img
                   ref={furn_tablelamp}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_r_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-tablelamp.png"
                   alt=""
                 />
                 <img
                   ref={furn_wallbookshelf}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_l_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-wallbookshelf.png"
                   alt=""
                 />
                 <img
                   ref={furn_wallpainting}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_r_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-wallpainting.png"
                   alt=""
                 />
                 <img
                   ref={furn_wallshelf}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_l_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-wallshelf.png"
                   alt=""
                 />
                 <img
                   ref={furn_wallstickies}
-                  class="absolute left-0 top-0"
+                  class={`absolute ${translate_r_52} top-0 opacity-0 transition duration-1000`}
                   src="src/assets/photos-furniture/furn-wallstickies.png"
                   alt=""
                 />
--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,4 +1,4 @@
-import { createEffect, onMount } from "solid-js";
+import { createEffect, createSignal, onMount } from "solid-js";
 import Topbar from "../Topbar";
 import LandingHeroSection from "./HeroSection";
 import LandingAnimationSection from "./LandingAnimationSection";
@@ -6,6 +6,7 @@ import LandingDirectToShop from "./LandingDirectToShop";
 import LandingProductsCarousel from "./LandingProductsCarousel";
 import LandingTestimonialsSection from "./LandingTestimonialsSection";
 import { createMediaQuery } from "@solid-primitives/media";
+import { throttle } from "@solid-primitives/scheduled";
 
 const LandingPage = () => {
   let topBar!: HTMLDivElement;
@@ -19,9 +20,16 @@ const LandingPage = () => {
 
   const isSmall = createMediaQuery("(max-width: 767px)");
 
+  const [animate, setAnimate] = createSignal(false);
+  const throttledGetCenteredChild = throttle(
+    (e: Event) => getCenteredChild(e),
+    150,
+  );
+
   onMount(() => {
     const calcedHeight = `calc(100vh - ${topBar.clientHeight}px)`;
 
+    myContents.addEventListener("scroll", throttledGetCenteredChild);
     myContents.style.setProperty("height", calcedHeight);
     landingHero.style.setProperty("height", calcedHeight);
     landingAnimation.style.setProperty("height", calcedHeight);
@@ -34,6 +42,36 @@ const LandingPage = () => {
     if (isSmall()) landingShop.style.setProperty("height", calcedHeight);
     else landingShop.style.removeProperty("height");
   });
+
+  function getCenteredChild(e: Event) {
+    //  can't use native elementFromPoint for better performance; it gets the topmost element
+    if (e.target instanceof HTMLElement) {
+      const rect = e.target.getBoundingClientRect();
+      for (const cell of e.target.children) {
+        if (cell instanceof HTMLElement) {
+          const cellRect = cell.getBoundingClientRect();
+          const bounds = [
+            rect.height / 2 - cellRect.height / 2,
+            rect.height / 2 + cellRect.height / 2,
+          ];
+          const centerScrollOffset =
+            cellRect.top - rect.top + cell.offsetHeight / 2;
+          if (
+            bounds[0] < centerScrollOffset &&
+            centerScrollOffset < bounds[1]
+          ) {
+            if (cell.id == "animation") {
+              setAnimate(true);
+              myContents.removeEventListener(
+                "scroll",
+                throttledGetCenteredChild,
+              );
+            }
+          }
+        }
+      }
+    }
+  }
   return (
     <div>
       <Topbar ref={topBar} />
@@ -42,7 +80,7 @@ const LandingPage = () => {
         class="flex w-screen flex-shrink-0 snap-y snap-mandatory flex-col overflow-y-auto"
       >
         <LandingHeroSection ref={landingHero} />
-        <LandingAnimationSection ref={landingAnimation} />
+        <LandingAnimationSection ref={landingAnimation} animate={animate()} />
         <LandingProductsCarousel ref={landingProducts} />
         <LandingTestimonialsSection ref={landingTestimonials} />
         <LandingDirectToShop ref={landingShop} />
