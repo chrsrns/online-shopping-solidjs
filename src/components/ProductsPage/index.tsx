5236dedcb4069f42801de3e90f2a4431d3990f45
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-14 09:58:55 +0800

    Updated conditional rendering of product items
    This implementation uses an `Option` to check if the data from the server is valid via the `ProductItem` class.

--- src/components/ProductsPage/index.tsx
+++ src/components/ProductsPage/index.tsx
@@ -9,11 +9,40 @@ import {
 } from "solid-js";
 import Topbar from "../Topbar";
 import ProductItemModal from "../ProductItemModal";
+import { Option, Some, None, match } from "oxide.ts";
+
+class ProductItem {
+  id: number;
+  iname: string;
+  img_link: string;
+  price: number;
+
+  constructor(id: number, iname: string, img_link: string, price: number) {
+    this.id = id;
+    this.iname = iname;
+    this.img_link = img_link;
+    this.price = price;
+  }
+
+  static fromData(data: any): Option<ProductItem> {
+    if (
+      typeof data.id === "number" &&
+      typeof data.iname === "string" &&
+      typeof data.img_link === "string" &&
+      typeof data.price === "number"
+    ) {
+      return Some(
+        new ProductItem(data.id, data.iname, data.img_link, data.price),
+      );
+    } else return None;
+  }
+}
 
 const fetchShopItems = async () => {
   const response = await fetch("http://127.0.0.1:8000/api/shopitems");
   return response.json();
 };
+
 const ProductsPage = () => {
   let topBar!: HTMLDivElement;
   const [shopItems] = createResource(fetchShopItems);
@@ -46,73 +75,75 @@ const ProductsPage = () => {
           </Match>
           <Match when={shopItems()}>
             <For each={shopItems()}>
-              {(item, index) => (
-                <Show
-                  when={
-                    typeof item.iname === "string" &&
-                    typeof item.img_link === "string" &&
-                    typeof item.price === "number"
-                  }
-                >
-                  <a
-                    href="#"
-                    class="group relative block overflow-hidden rounded-lg shadow-lg"
-                    onClick={() => {
-                      console.log("clicked");
-                      setShowModal(true);
-                    }}
-                  >
-                    <button class="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
-                      <span class="sr-only">Wishlist</span>
-
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="h-4 w-4"
+              {(item) => {
+                const productItemFromData = ProductItem.fromData(item);
+                return match(productItemFromData, {
+                  Some: (productItem) => {
+                    return (
+                      <a
+                        href="#"
+                        class="group relative block overflow-hidden rounded-lg shadow-lg"
+                        onClick={() => {
+                          console.log("clicked");
+                          setShowModal(true);
+                        }}
                       >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-                        />
-                      </svg>
-                    </button>
+                        <button class="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75">
+                          <span class="sr-only">Wishlist</span>
 
-                    <img
-                      src={item.img_link}
-                      alt=""
-                      class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-                    />
-
-                    <div class="relative bg-white p-6 dark:bg-black_olive">
-                      {/* <span class="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium"> */}
-                      {/*   {" "} */}
-                      {/*   New{" "} */}
-                      {/* </span> */}
-
-                      <h3 class="text-lg font-medium text-gray-900 dark:text-white">
-                        {item.iname}
-                      </h3>
-
-                      <p class="mt-1.5 text-sm text-gray-700 dark:text-white">
-                        {Number(item.price).toLocaleString("en", {
-                          style: "currency",
-                          currency: "PHP",
-                        })}
-                      </p>
-
-                      <form class="mt-4">
-                        <button class="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105">
-                          Add to Cart
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="h-4 w-4"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                            />
+                          </svg>
                         </button>
-                      </form>
-                    </div>
-                  </a>
-                </Show>
-              )}
+
+                        <img
+                          src={productItem.img_link}
+                          alt=""
+                          class="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+                        />
+
+                        <div class="relative bg-white p-6 dark:bg-black_olive">
+                          {/* <span class="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium"> */}
+                          {/*   {" "} */}
+                          {/*   New{" "} */}
+                          {/* </span> */}
+
+                          <h3 class="text-lg font-medium text-gray-900 dark:text-white">
+                            {productItem.iname}
+                          </h3>
+
+                          <p class="mt-1.5 text-sm text-gray-700 dark:text-white">
+                            {Number(productItem.price).toLocaleString("en", {
+                              style: "currency",
+                              currency: "PHP",
+                            })}
+                          </p>
+
+                          <form class="mt-4">
+                            <button class="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105">
+                              Add to Cart
+                            </button>
+                          </form>
+                        </div>
+                      </a>
+                    );
+                  },
+                  None: () => {
+                    return <div>Error</div>;
+                  },
+                });
+              }}
             </For>
           </Match>
         </Switch>
