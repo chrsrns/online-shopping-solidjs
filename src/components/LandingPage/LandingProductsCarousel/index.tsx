13ef3c757dcbdbfe48b56aad36432b0e241d26fa
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-23 21:56:24 +0800

    Added initial responsive impl of product descriptions

--- src/components/LandingPage/LandingProductsCarousel/index.tsx
+++ src/components/LandingPage/LandingProductsCarousel/index.tsx
@@ -1,8 +1,13 @@
-import { createResource, For } from "solid-js";
+import { createResource, For, Match, Show, Switch } from "solid-js";
 import { ComponentRefProps } from "../../../ComponentRefProps";
+import { FaSolidSpinner } from "solid-icons/fa";
 
 const fetchShopItems = async () => {
   const response = await fetch("http://127.0.0.1:8000/api/shopitems");
+  return response.json();
+};
+const fetchShopItemDesc = async (id: number) => {
+  const response = await fetch(`http://127.0.0.1:8000/api/shopitemdescs/${id}`);
   return response.json();
 };
 const LandingProductsCarousel = (props: ComponentRefProps) => {
@@ -16,7 +21,7 @@ const LandingProductsCarousel = (props: ComponentRefProps) => {
         <For each={shopItems()}>
           {(item, index) => (
             <div class="flex-grow-1 flex h-full w-full flex-shrink-0 basis-full snap-center flex-col items-center justify-center overflow-clip rounded-md bg-timberwolf dark:bg-black_olive dark:text-white">
-              <div class="flex w-full flex-grow overflow-auto">
+              <div class="inline-flex w-full flex-grow flex-wrap overflow-auto">
                 <div class="rounded-lg bg-timberwolf p-6 dark:bg-black_olive dark:text-white">
                   <div class="aspect-square h-48 w-full sm:h-64">
                     <img
@@ -36,6 +41,44 @@ const LandingProductsCarousel = (props: ComponentRefProps) => {
                       })}
                     </p>
                   </div>
+                </div>
+                <div class="w-min min-w-72 flex-grow p-6 dark:bg-black_olive-300">
+                  {(() => {
+                    const [descsFetched] = createResource(
+                      item.id,
+                      fetchShopItemDesc,
+                    );
+
+                    return (
+                      <Switch>
+                        <Match when={descsFetched.loading}>
+                          <div class="flex w-full justify-center pb-6 dark:text-white">
+                            <FaSolidSpinner class="animate-fade_in_slow_loading text-lg" />
+                          </div>
+                        </Match>
+                        <Match when={descsFetched.error}>
+                          <div class="flex w-full justify-center pb-6 dark:text-white">
+                            <span>Network error. Please try again.</span>
+                          </div>
+                        </Match>
+                        <Match when={descsFetched()}>
+                          <For each={descsFetched()}>
+                            {(descItem) => {
+                              return (
+                                <Show
+                                  when={typeof descItem.content === "string"}
+                                >
+                                  <p class="mb-1.5 dark:text-white">
+                                    {descItem.content}
+                                  </p>
+                                </Show>
+                              );
+                            }}
+                          </For>
+                        </Match>
+                      </Switch>
+                    );
+                  })()}
                 </div>
               </div>
             </div>
