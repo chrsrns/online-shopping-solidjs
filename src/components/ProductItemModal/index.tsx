6660ed92ed07a1cc16fd0f329d7be313bdf661dc
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-26 16:20:42 +0800

    Changed max width of product modal on wider screens

--- src/components/ProductItemModal/index.tsx
+++ src/components/ProductItemModal/index.tsx
@@ -75,7 +75,7 @@ const ProductItemModal = (props: ProductItemModalProps) => {
 
       <div class="pointer-events-none fixed inset-0 z-10 h-screen w-screen">
         <div class="flex min-h-full flex-col items-center justify-center p-4 text-center sm:items-center sm:p-0">
-          <div class="pointer-events-auto relative flex flex-[1_1_1px] flex-grow transform animate-fade_in_fast flex-col overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg dark:bg-walnut_brown-400">
+          <div class="pointer-events-auto relative flex flex-[1_1_1px] flex-grow transform animate-fade_in_fast flex-col overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl lg:max-w-2xl dark:bg-walnut_brown-400">
             <div class="w-full flex-grow overflow-auto">
               <img
                 src={props.productItem.img_link}
