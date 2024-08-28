bd6c6fbb581f69788b0b97d49b0377930e64aa5f
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-28 22:25:09 +0800

    Changed hero section text

--- src/components/LandingPage/HeroSection/index.tsx
+++ src/components/LandingPage/HeroSection/index.tsx
@@ -12,8 +12,8 @@ const LandingHeroSection = (props: ComponentRefProps) => {
         />
         <div class="absolute inset-0 bg-black bg-opacity-50"></div>
         <div class="absolute inset-0 flex flex-col items-center justify-center">
-          <h1 class="text-4xl font-bold text-white">Hero Sample Text</h1>
-          <p class="mt-4 text-xl text-white">This is a sample text</p>
+          <h1 class="text-4xl font-bold text-white">FurniCo</h1>
+          <p class="mt-4 text-xl text-white">Mollit non deserunt ullamco!</p>
         </div>
       </div>
     </div>
