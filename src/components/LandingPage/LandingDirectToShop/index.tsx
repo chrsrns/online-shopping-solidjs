5605672ac0852f1231e288b2841cf1387873c207
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-08-30 23:22:24 +0800

    Fixed mobile UI of direct to shop section

--- src/components/LandingPage/LandingDirectToShop/index.tsx
+++ src/components/LandingPage/LandingDirectToShop/index.tsx
@@ -23,7 +23,7 @@ const LandingDirectToShop = (props: ComponentRefProps) => {
         class="flex justify-center lg:h-auto dark:bg-walnut_brown-400"
       >
         <div class="my-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-          <div class="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
+          <div class="grid grid-cols-1 justify-items-center gap-y-8 lg:grid-cols-2 lg:items-center lg:gap-x-16">
             <div class="mx-auto max-w-lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
               <h2 class="text-3xl font-bold sm:text-4xl dark:text-gray-100">
                 All your furniture needs...
@@ -38,20 +38,20 @@ const LandingDirectToShop = (props: ComponentRefProps) => {
 
               <A
                 href="/shop"
-                class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400 dark:bg-timberwolf-500 dark:text-black hover:dark:bg-timberwolf-300 hover:dark:text-white"
+                class="mt-8 inline-block rounded bg-yellow-400 px-12 py-3 text-sm font-medium text-black"
               >
                 Shop now!
               </A>
             </div>
 
-            <div class="grid grid-cols-2 gap-4 sm:grid-cols-3">
+            <div class="grid w-fit grid-cols-2 justify-items-center gap-4 sm:grid-cols-3">
               <For each={shopItems()}>
                 {(item, index) => {
-                  if (index() > 3) return <></>;
+                  if (index() > 2) return <></>;
                   return (
                     <A
                       href={`/shop?product_id=${item.id}`}
-                      class="block aspect-square max-w-44 overflow-clip rounded-xl border shadow-sm hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+                      class="block aspect-square w-32 overflow-clip rounded-xl border shadow-sm hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring sm:w-36"
                     >
                       <img class="h-full object-cover" src={item.img_link} />
 
@@ -63,7 +63,7 @@ const LandingDirectToShop = (props: ComponentRefProps) => {
                 }}
               </For>
               <A
-                class="flex aspect-square max-w-44 items-center justify-center overflow-clip rounded-xl border shadow-sm hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring"
+                class="flex aspect-square w-32 items-center justify-center overflow-clip rounded-xl border shadow-sm hover:ring-1 hover:ring-gray-200 focus:outline-none focus:ring sm:w-36"
                 href="/shop"
               >
                 <h2 class="font-bold dark:text-timberwolf-800">And more...</h2>
